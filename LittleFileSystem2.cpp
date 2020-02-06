e2ee8a4558655ff68d95723114ac369e6df9a4a1
Pavel Slama <info@pavelslama.cz>
2020-02-06 17:33:31 +0100

    mbed 5.15 CRC update

--- LittleFileSystem2.cpp
+++ LittleFileSystem2.cpp
@@ -24,8 +24,9 @@ namespace mbed {
 
 extern "C" uint32_t lfs2_crc(uint32_t crc, const void *buffer, size_t size)
 {
-    uint32_t initial_xor = crc;
-    MbedCRC<POLY_32BIT_REV_ANSI, 32> ct(initial_xor, 0x0, true, false);
+    uint32_t initial_xor = lfs2_rbit(crc);
+    MbedCRC<POLY_32BIT_ANSI, 32, CrcMode::TABLE> ct(initial_xor, 0x0, true, true);
+
     ct.compute((void *)buffer, size, &crc);
     return crc;
 }
